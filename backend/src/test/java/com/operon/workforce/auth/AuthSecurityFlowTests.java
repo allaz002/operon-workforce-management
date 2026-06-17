48f4e39cef13971b2d75a97b08529a93b34ce549
Alexander Lazarev <xxragepowerxxxxragepowerxx@gmail.com>
2026-06-17 14:39:31 +0200

    Add: admin login test flow

--- backend/src/test/java/com/operon/workforce/auth/AuthSecurityFlowTests.java
+++ backend/src/test/java/com/operon/workforce/auth/AuthSecurityFlowTests.java
@@ -1,18 +1,20 @@
 package com.operon.workforce.auth;
 
-import com.operon.workforce.user.RegisterUserRequest;
-import com.operon.workforce.user.UserApprovalStatus;
-import com.operon.workforce.user.UserRepository;
+import com.operon.workforce.user.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
+import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,6 +31,9 @@ class AuthSecurityFlowTests {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @BeforeEach
     public void cleanDatabase() {
@@ -53,7 +58,7 @@ class AuthSecurityFlowTests {
                 .andExpect(status().is(201))
                 .andExpect(jsonPath("$.approvalStatus").value(UserApprovalStatus.PENDING.toString()))
                 .andExpect(jsonPath("$.passwordHash").doesNotExist())
-                .andExpect(jsonPath("$.passwordHash").doesNotExist());
+                .andExpect(jsonPath("$.password").doesNotExist());
 
         LoginRequest loginRequest = new LoginRequest(
                 "max@example.com",
@@ -69,7 +74,42 @@ class AuthSecurityFlowTests {
                 .andExpect(status().isForbidden())
                 .andExpect(jsonPath("$.error").value("Forbidden"))
                 .andExpect(jsonPath("$.message").value(UserNotApprovedException.errorMessage));
+    }
 
+    @Test
+    public void adminCanLoginAndReceivesToken() throws Exception {
+        String adminFirstName = "Administrator";
+        String adminLastName = "User";
+        String adminEmail = "admin@operon.local";
+        String adminPassword = "admin12345";
+        String adminPasswordHash = passwordEncoder.encode(adminPassword);
 
+        User admin = new User(adminFirstName, adminLastName, adminEmail, adminPasswordHash, UserRole.ADMIN);
+        admin.approve();
+        userRepository.save(admin);
+
+        String adminToken = loginAndReturnToken(adminEmail, adminPassword);
+        assertThat(adminToken).isNotBlank();
+    }
+
+    private String loginAndReturnToken(String email, String password) throws Exception {
+        LoginRequest loginRequest = new LoginRequest(
+                email,
+                password
+        );
+        String jsonLoginRequest = objectMapper.writeValueAsString(loginRequest);
+
+        RequestBuilder postLogin = post("/api/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonLoginRequest);
+
+        MvcResult loginResult = mockMvc.perform(postLogin)
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$.token").exists())
+                .andReturn();
+
+        String loginResponseBody = loginResult.getResponse().getContentAsString();
+        JsonNode loginResponseJson = objectMapper.readTree(loginResponseBody);
+        return loginResponseJson.get("token").asString();
     }
 }
